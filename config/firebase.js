9698e65a7b2306491f69eefe48eb2cb255cbbfb8
Aayush mittal <ayushmittal844@gmail.com>
2026-02-07 04:40:18 +0530

    Fix CORS and Firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,15 +1,28 @@
 import admin from "firebase-admin";
 
-if (!process.env.FIREBASE_ADMIN_SDK) {
-  throw new Error("FIREBASE_ADMIN_SDK environment variable not set");
-}
-
-// Parse Firebase service account JSON from env variable
 let serviceAccount;
-try {
-  serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
-} catch (err) {
-  throw new Error("Invalid JSON in FIREBASE_ADMIN_SDK environment variable");
+
+if (process.env.FIREBASE_ADMIN_SDK) {
+  try {
+    serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_SDK);
+  } catch (err) {
+    throw new Error("Invalid JSON in FIREBASE_ADMIN_SDK environment variable");
+  }
+} else if (process.env.FIREBASE_PRIVATE_KEY && process.env.FIREBASE_CLIENT_EMAIL) {
+  serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+  };
+} else {
+  throw new Error("FIREBASE_ADMIN_SDK or individual FIREBASE_* environment variables not set");
 }
 
 // Fix private key newlines (important)
